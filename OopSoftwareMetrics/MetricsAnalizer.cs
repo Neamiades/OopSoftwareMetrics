737092fd9f357fe0de77e2e144af0faa62a371ae
Serhii Yakhin <Neamiades@gmail.com>
2017-12-18 01:45:10 +0200

    +

---
+++ OopSoftwareMetrics/MetricsAnalizer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OopSoftwareMetrics
+{
+    class MetricsAnalizer
+    {
+        private Dictionary<string, ClassNode> _classes;
+
+        private readonly ClassNode _root;
+
+        private readonly Object _thisLock = new Object();
+
+        private const string Pattern = @"class\s*(\w+)(?:<\s*\w+(?:\s*,\s*\w+)*>)?(?:\s*:\s*(\w+))?";
+
+        private readonly Regex _regex = new Regex(Pattern);
+
+        public MetricsAnalizer()
+        {
+            _root = new ClassNode(null);
+            _classes = new Dictionary<string, ClassNode>();
+        }
+
+        public void GetModuleMetrics(string pathToModule)
+        {
+            var dirInfo = new DirectoryInfo(pathToModule);
+
+
+            foreach (var file in Directory.GetFiles(dirInfo.FullName, "*.cs"))
+                GetClassTree(file);
+
+            foreach (var directory in Directory.GetDirectories(dirInfo.FullName))
+                GetModuleMetrics(directory);
+
+            //Parallel.ForEach(Directory.GetFiles(dirInfo.FullName, "*.cs"), GetClassTree);
+            //Parallel.ForEach(Directory.GetDirectories(dirInfo.FullName), GetModuleMetrics);
+        }
+
+        private void GetClassTree(string pathToFile)
+        {
+            using (var streamReader = new StreamReader(pathToFile))
+            {
+                string file = streamReader.ReadToEnd();
+
+                foreach (Match match in _regex.Matches(file))
+                {
+                    if (!String.IsNullOrEmpty(match.Groups[2].Value) && !match.Groups[2].Value.StartsWith("I"))
+                    {
+                        lock (_thisLock)
+                        {
+                            ClassNode classNode;
+                            if ((classNode = _root.FindChild(match.Groups[2].Value)) != null)
+                            {
+                                if (classNode.ContainsChild(match.Groups[1].Value))
+                                    continue;
+
+                                classNode.AddChild(match.Groups[1].Value);
+                            }
+                            else if ((classNode = _root.FindChild(match.Groups[1].Value)) != null)
+                            {
+                                _root.AddChild(match.Groups[2].Value)
+                                    .AddChild(classNode, match.Groups[1].Value);
+
+                                _root.RemoveChild(match.Groups[1].Value);
+                            }
+                            else
+                            {
+                                _root.AddChild(match.Groups[2].Value).AddChild(match.Groups[1].Value);
+                            }
+                        }
+                    }
+                    else lock (_thisLock)
+                    {
+                        if (_root.FindChild(match.Groups[1].Value) == null)
+                        {
+                            _root.AddChild(match.Groups[1].Value);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
--- OopSoftwareMetrics/Program.cs
+++ OopSoftwareMetrics/Program.cs
@@ -1,15 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
 
 namespace OopSoftwareMetrics
 {
     class Program
     {
-        static void Main(string[] args)
+        //        static void Main()
+        //        {
+        //            string input = @"public class Aon<T,K> : IFoo, Gen, INoo<T>
+        //{
+        //	int d;
+        //}
+
+        //public class Aon<T,K> : Gen<T,K>, IGen, INoo<T>
+        //{
+        //  private class Foo
+        //  {
+        //  }
+        //	int d;
+        //}
+
+        //public class Aon<T,K>
+        //{
+        //	int d;
+        //}";
+
+        //            string pattern = @"class\s*(\w+)(?:<\s*\w+(?:\s*,\s*\w+)*>)?(?:\s*:\s*(\w+))?";
+        //            Regex regex = new Regex(pattern);
+
+        //            foreach (Match match in regex.Matches(input))
+        //            {
+        //                Console.WriteLine(match.Groups[0].Value);
+        //                var x = String.IsNullOrWhiteSpace(match.Groups[2].Value);
+        //                Console.WriteLine(match.Groups[2].Value);
+
+        //                Console.WriteLine(match.Value);
+        //            }
+        //        }
+
+        private const string ResultString = "From module {0} we get this metrics:\n" +
+                                            "Number of code lines: {1}\n" +
+                                            "Number of empty code lines: {2}\n" +
+                                            "Number of physical code lines: {3}\n" +
+                                            "Number of logical code lines: {4}\n" +
+                                            "Number of comment lines: {5}\n" +
+                                            "Comment percantage: {6:0.0}%\n";
+
+        static void Main()
         {
+            string input;
+            var metricsAnalizer = new MetricsAnalizer();
+
+            while ((input = ReadLine()) != null && input != "exit")
+            {
+                try
+                {
+                    var x = Stopwatch.StartNew();
+                    metricsAnalizer.GetModuleMetrics(input);
+                    x.Stop();
+                    WriteLine(x.ElapsedMilliseconds);
+                }
+                catch (Exception e)
+                {
+                    WriteLine(e);
+                    continue;
+                }
+
+                OutPutMetrics(input, metricsAnalizer);
+            }
+        }
+
+        private static void OutPutMetrics(string input, MetricsAnalizer metricsAnalizer)
+        {
+            //WriteLine(ResultString, Path.GetDirectoryName(input), metricsAnalizer.CodeLinesCount,
+            //    metricsAnalizer.EmptyCodeLinesCount, metricsAnalizer.PhysicalCodeLinesCount,
+            //    metricsAnalizer.LogicalCodeLinesCount, metricsAnalizer.CommentCodeLinesCount,
+            //    metricsAnalizer.CommentPercent);
         }
     }
 }
